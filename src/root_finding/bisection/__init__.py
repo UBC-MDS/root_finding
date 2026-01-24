a09e4bfebd8ce8d8adba3af2020b973a1ea81f28
Coachyyds <puli20030306@gmail.com>
2026-01-24 13:21:29 -0800

    added init.py in bisection

---
+++ src/root_finding/bisection/__init__.py
@@ -0,0 +1,6 @@
+"""Bisection method for root finding."""
+
+from .bisection import bisection
+from .bisection_find_roots import bisection_find_roots
+
+__all__ = ["bisection", "bisection_find_roots"]
